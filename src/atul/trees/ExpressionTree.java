11d56438fa9f038435c4a42cb422dda60af73516
agosain <agosain@brocade.com>
2017-06-11 00:33:36 -0700

    Initial version

---
+++ src/atul/trees/ExpressionTree.java
@@ -0,0 +1,98 @@
+package atul.trees;
+
+import java.util.Stack;
+
+/**
+ * Created by agosain on 6/10/2017.
+ */
+public class ExpressionTree<T> {
+    public String toString(ExprNode<T> node){
+        String buffer = "";
+        if(node==null)
+            return "";
+        if(node.left !=null) buffer += "(";
+        buffer += toString(node.left);
+        buffer += node.toString();
+        buffer += toString(node.right);
+        if(node.right!=null) buffer += ")";
+        return buffer;
+    }
+
+    public int eval(ExprNode<T> node){
+        int result = 0;
+        if(node.getType()== ExprNode.TYPE.OPERATOR){
+            int leftResult = eval(node.left);
+            int rightResult = eval(node.right);
+            switch((String)node.getData()) {
+                case "+":
+                    result = leftResult+rightResult;
+                    break;
+                case "*":
+                    result = leftResult*rightResult;
+                    break;
+                case "-":
+                    result = leftResult - rightResult;
+                    break;
+                case "/":
+                    result = leftResult / rightResult;
+                    break;
+            }
+        }else{
+            return Integer.parseInt((String)node.getData());
+        }
+
+        return result;
+    }
+
+    public static void main(String args[]){
+        ExprNode node = new ExprNode("+", ExprNode.TYPE.OPERATOR);
+        node.left = new ExprNode("*", ExprNode.TYPE.OPERATOR);
+        node.left.left = new ExprNode("2", ExprNode.TYPE.NUMBER);
+        node.left.right = new ExprNode("3", ExprNode.TYPE.NUMBER);
+        node.right = new ExprNode("/", ExprNode.TYPE.OPERATOR);
+        node.right.left = new ExprNode("36", ExprNode.TYPE.NUMBER);
+        node.right.right = new ExprNode("6", ExprNode.TYPE.NUMBER);
+
+        System.out.println(new ExpressionTree<String>().toString(node));
+        System.out.println(new ExpressionTree<String>().eval(node));
+
+    }
+}
+
+class ExprNode<T>{
+    private TreeNode<T> treeNode;
+    private TYPE type;
+    public ExprNode<T> left;
+    public ExprNode<T> right;
+
+    public enum TYPE {
+        OPERATOR,
+        NUMBER
+    };
+
+    public String toString(){
+        return treeNode.getData().toString();
+    }
+
+    public void addLeft(ExprNode<T> node, TYPE type){
+        treeNode.left = new TreeNode(node.getData());
+        this.type = type;
+        left = new ExprNode<T>(node.getData(), type);
+    }
+    public void addRight(ExprNode<T> node, TYPE type){
+        treeNode.right = new TreeNode(node.getData());
+        this.type = type;
+        right = new ExprNode<T>(node.getData(), type);
+    }
+    public ExprNode(T data, TYPE type){
+        treeNode = new TreeNode<T>(data);
+        this.type = type;
+    }
+
+    public T getData(){
+        return treeNode.getData();
+    }
+    public TYPE getType(){
+        return type;
+    }
+}
